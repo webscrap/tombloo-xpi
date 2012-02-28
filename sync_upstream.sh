e31dbf41df33cf429b0e30a12727d14a3fc6ca26
Eoetct Nahn <eotect@gmail.com>
2012-02-28 19:35:57 +0800

    Update script sync_upstream.sh

--- sync_upstream.sh
+++ sync_upstream.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-EX="upstream/xpi/install.rdf upstream/xpi/chrome/content/quickPostForm.xul"
-exec tar -c --exclude="$EX" upstream/xpi/ | tar --strip-components=2 -xv
+EX="--exclude upstream/xpi/install.rdf --exclude=upstream/xpi/chrome/content/quickPostForm.xul"
+exec tar -c $EX upstream/xpi/ | tar --strip-components=2 -xv
